96f34f3ccfe5c69477eee68c74e3c71431805486
Eric Walsh <ericmatthewwalsh@gmail.com>
2018-03-27 23:29:02 -0700

    add exceptions; continue with ensure_prices (add exchange_map and timestamp_map)

---
+++ app/controllers/concerns/exceptions.rb
@@ -0,0 +1,60 @@
+# currently borrowed from carnitas...
+# look into abstracting into a gem?
+module Exceptions
+  BAD_REQUEST = 400
+  UNAUTHORIZED = 401
+  FORBIDDEN = 403
+  NOT_FOUND = 404
+  BAD_SAVE = 422
+  RATE_LIMIT = 429
+
+  class BaseError < StandardError
+    attr_reader :status
+    #
+    def initialize(message = 'Bad Request', status = BAD_REQUEST) # string, integer
+      @status = status
+      super(message)
+    end
+
+    private
+
+    def ar_error_messages(record) # hash
+      record.map do |field, errors|
+        "#{field.upcase} - #{errors.join(',')}"
+      end.join('; ')
+    end
+
+  end
+
+  class UnauthorizedError < BaseError
+    def initialize(message = 'Unauthorized') # string
+      super(message, UNAUTHORIZED)
+    end
+  end
+
+  class ForbiddenError < BaseError
+    def initialize(message = 'Forbidden') # string
+      super(message, FORBIDDEN)
+    end
+  end
+
+  class NotFoundError < BaseError
+    def initialize(message = 'Not Found') # string
+      super(message, NOT_FOUND)
+    end
+  end
+
+  class BadSaveError < BaseError
+    def initialize(record) # hash
+      message = record.nil? ? 'Bad Save' : ar_error_messages(record)
+      super(message, BAD_SAVE)
+    end
+  end
+
+  class ApiRateLimitError < BaseError
+    def initialize(message = 'API Rate Limit Reached', status = RATE_LIMIT) # string, integer
+      super(message, status)
+    end
+  end
+
+end
--- app/services/loaders/get_prices_and_volumes.rb
+++ app/services/loaders/get_prices_and_volumes.rb
@@ -7,8 +7,11 @@ module Loaders
       # start time timestamp, end time timestamp, minute interval
       # ::Loaders::GetPricesAndVolumes.perform(1521516300, 1521516600, 1)
       def perform(st, et, intvl = 1) # int, int, int
-        prices = ensure_prices(load_prices(st, et, intvl))
-        volumes = ensure_volumes(load_volumes(st, et, intvl))
+        unless validate_inputs(st, et, intvl)
+          raise ::Exceptions::BaseError, 'invalid input params'
+        end
+        prices = ensure_prices(st, et, intvl)
+        volumes = ensure_volumes(st, et, intvl)
 
         merge_prices_and_volumes(prices, volumes)
       end
@@ -19,10 +22,22 @@ module Loaders
         #
       end
 
-      def ensure_prices(sql_prices) # PG::Result (enumerable)
-        sql_prices
+      # ::Loaders::GetPricesAndVolumes.send(:ensure_prices, 1521516300, 1521516600, 1)
+      def ensure_prices(st, et, intvl)  # int, int, int
+        exchange_map = exchange_map(timestamp_map(st, et, intvl))
+        sql_prices = load_prices(st, et, intvl)
+        grouped_prices = timestamp_map(st, et, intvl, [])
+
+        # {"exchange"=>"binance", "symbol"=>"ADA/BTC", "base_asset"=>"ADA", "quote_asset"=>"BTC", "times"=>1521516360.0, "price"=>2.226e-05}
+        sql_prices.each do |price_hash|
+          exchange_map[price_hash['exchange']][price_hash['times'].to_i] = true
+          grouped_prices[price_hash['times'].to_i].push(price_hash)
+        end
+
+        grouped_prices
       end
 
+      # ::Loaders::GetPricesAndVolumes.send(:load_prices, 1521516300, 1521516600, 1)
       def load_prices(st, et, intvl)  # int, int, int
         execute_query(
           <<~HEREDOC
@@ -34,8 +49,9 @@ module Loaders
         )
       end
 
-      def ensure_volumes(sql_volumes) # PG::Result (enumerable)
-        sql_volumes
+      def ensure_volumes(st, et, intvl)  # int, int, int
+        exchange_map = exchange_map(timestamp_map(st, et, intvl))
+        sql_volumes = load_volumes(st, et, intvl)
       end
 
       def load_volumes(st, et, intvl)  # int, int, int
@@ -47,6 +63,39 @@ module Loaders
             )
           HEREDOC
         )
+      end
+
+      def exchange_map(timestamp_map) # hash
+        exchange_map = {}
+        EXCHANGES.each do |exchange|
+          exchange_map[exchange] = timestamp_map
+        end
+        exchange_map
+      end
+
+      def timestamp_map(st, et, intvl, val = false) # int, int, int, any
+        timestamp_map = {}
+        intvl_in_seconds = intvl.to_i * 60
+
+        count = st.to_i + intvl_in_seconds
+        while count <= et.to_i
+          timestamp_map[count] = val
+          count += intvl_in_seconds
+        end
+
+        timestamp_map
+      end
+
+      def validate_inputs(st, et, intvl) # int, int, int
+        timestamp_delta = et.to_i - st.to_i
+        intvl_in_seconds = intvl.to_i * 60
+        if timestamp_delta <= 0 || intvl_in_seconds <= 0
+          false
+        elsif timestamp_delta % intvl_in_seconds != 0
+          false
+        else
+          true
+        end
       end
 
     end
